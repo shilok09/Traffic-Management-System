f2a78e33c735231a6e469cc2a961cc311f20c687
sabeelnadim <i232649@isb.nu.edu.pk>
2024-12-08 11:16:07 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,18 +10,107 @@ struct road {
     road* next;      
     road(char d,int s,road* nextRoad=nullptr):dest(d),dist(s),next(nextRoad){}
 };
+class vehicle
+{   public:
+    string V;
+    string path;
+    string curr;
+    int l=1;
+    vehicle(string v="", string p="")
+    {
+        V=v;
+        path=p;
+        for(int i=0; i<2; i++)
+        {
+            if(p[i]!='\0')
+            {
+            curr+=p[i];
+            }
+        }
+        cout<<"current: "<<curr<<endl;
+       
+    }
+    void change_Current()
+    {
+        curr="";
+       for(int i=l; i<2+l; i++)
+        {
+            if(path[i]!='\0')
+            {
+            curr+=path[i];
+            }
+        }
+        cout<<"current: "<<curr<<endl;
+        l++;
+    }
+    string current_path()
+    {
+        return curr;
+    }
+};
+struct track{
+    string road;     
+    int vehicleCount; 
+    track *next;  
+    track(){road="",vehicleCount=0,next=nullptr;}
+    track(string r, int c):road(r),vehicleCount(c),next(nullptr) {}
+};
+class CongestionMonitoring{
+private:
+    track* table[27];
+    int hashFunction(char src, char dest) {
+        int hash = src + dest;
+        return hash%27;
+}
+public:
+    CongestionMonitoring(){
+        for (int i=0;i<27; i++) {
+            table[i]=nullptr;
+        }
+    }
+    void add(char src,char dest) {
+       string roadKey = string(1, src) + " to " + string(1, dest);//concating starting vertex and ending vertex i.e A + B = AB
+        int index=hashFunction(src,dest);
 
+        track* temp=table[index];
+        while(temp){
+            if(temp->road == roadKey){
+                temp->vehicleCount++;
+                return;
+            }
+            temp=temp->next;
+        }
+        //IF NOT FOUND
+        track* nawaTrack=new track(roadKey, 1);
+        nawaTrack->next=table[index];
+        table[index]=nawaTrack;
+    }
+    void display(){
+        for(int i=0;i<27;i++) {
+            track* temp=table[i];
+            while(temp){
+                cout<<temp->road<<" | Vehicles: "<<temp->vehicleCount<<endl;
+                temp=temp->next;
+            }
+        }
+    }
+};
 class roadNetworks{
 private:
     int numOfVertex;
     road **roadKiList;
+    int T_vehicle=32;
+    vehicle*v;
 
 public:
     roadNetworks(){numOfVertex=0;roadKiList=nullptr;}  
-    roadNetworks(int n){  
+    roadNetworks(int n)
+    {  
+        v=new vehicle[32];
         numOfVertex = n;
         roadKiList = new road*[n];  //initialising list for storing graph in adjacency list manner
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             roadKiList[i] = nullptr; 
         }
     }
@@ -62,7 +151,7 @@ int index=-1;
 return index;
 }
 
-void ShortestPath(char start,char end) 
+string ShortestPath(char start,char end) 
 {
 int s=start-'A'; 
 int e=end-'A';
@@ -78,6 +167,7 @@ for(int i=0;i<numOfVertex;i++)
 dist[s]=0;           
 for(int c=0;c<numOfVertex;c++) 
     {
+        
         int u=min(dist,v,numOfVertex);
         v[u]=true; 
         if(u==e)//if end vertex is found then break
@@ -103,48 +193,114 @@ for(int c=0;c<numOfVertex;c++)
 
     temp=temp->next;
     }
-
     }
 if(dist[e]==1e9)
 {
   cout<<"No path was found."<<endl;
+  return "";
 }
 else 
 {
 cout<<"Shortest distance:"<<dist[e]<<endl;
 char a=end;
-int i=-1;
-a=end;
-i=-1;
-char s1=start;
-int s2;
-char s3=end;
-cout<<start;
-while(a!=p[end-'A']) 
-{       
-        s3=end;
-        cout<<"->";
-        while(p[i]!=s1)
-        {
-            s2=i;
-            i=s3-'A';
-            s3=p[i];
-        }
-        a=p[s2];
-        cout<<a; 
-        s1=a;
+int i;
+string V;
+while(a!='\0') 
+{
+     V+=a;
+       i=a-'A';
+       a=p[i];
 }
-cout<<"->";
-cout<<end<<endl;
-
+string temp;
+int j=0;
+int size=0;
+//reverse string
+while(V[size]!='\0')
+{
+    size++;
+}
+while(size>0)
+{
+    temp+=V[size-1];
+    j++;
+    size--;
+}
+V=temp;
+//cout<<V;
 delete[] dist;
-    delete[] p;
-    delete[] v;
+delete[] p;
+delete[] v;
+return V;
      }
- }
-};
+}
 
-void read_and_Build(roadNetworks &roads){
+void read_vehicle() {
+    ifstream file("vehicles.csv");
+    string line;
+
+    if (!file.is_open()) {
+        cout << "Error in opening file..." << endl;
+        return;
+    }
+
+    getline(file, line); 
+    int index = 0;
+    while (getline(file, line)) 
+    {
+        stringstream ss(line);
+        string vehicle_ID;
+        string src;
+        string dest;
+
+        getline(ss, vehicle_ID, ',');  
+        getline(ss, src, ',');        
+        getline(ss, dest, ',');        
+
+        char start = src[0];
+        char end = dest[0];
+        string path=ShortestPath(start, end);
+         
+            v[index]=vehicle(vehicle_ID, path);
+            cout<<"Vehicle " <<vehicle_ID << ": Path = " <<path<<endl;
+            index++;
+        
+    }
+
+    file.close();
+}
+void changeCurrent(int i)
+{
+    v[i].change_Current();
+}
+void read_And_addVehicle(CongestionMonitoring &rastyyy)
+ {
+    for (int i = 0; i < 31; i++)
+     {  
+        string vehiclePath = v[i].path;
+        int pathlength;
+        int pathLength = vehiclePath.length();
+        for(int j=0; j<pathLength-1;j++)
+         {  
+            char src=vehiclePath[j];
+            char dest=vehiclePath[j + 1];
+            rastyyy.add(src, dest);  
+        }
+    }
+}
+void emergencyVehicleRouting()
+{
+        char start, end;
+        cout<<"Enter start and end intersections for emergency vehicle: ";
+        cin>>start; 
+        cin>>end;
+        cout << "Emergency Vehicle is being routed..." << endl;
+        string path=ShortestPath(start, end);
+        cout << "Emergency Vehicle path: " << path << endl;
+}
+
+};
+void read_and_Build(roadNetworks &roads)
+{
     ifstream file("road_network.csv");
     string line;
     
@@ -168,19 +324,16 @@ void read_and_Build(roadNetworks &roads){
 
         roads.addRoads(src, dest, dist); 
     }
-
     file.close();
 }
-int main(){
-  bool exit= false;
-  RoadClosure closure;
-  closure.loadData();
-  TrafficSignalManagement traffic;
-  traffic.loadData();
-  int totalVertexs = 26; 
-  roadNetworks city(totalVertexs);  
-  read_and_Build(city);  
-  while(!exit){
+
+int main() {
+    int totalVertexs = 26; 
+    roadNetworks city(totalVertexs);  
+    read_and_Build(city); 
+    city.displayRoad(); 
+  while(!exit)
+  {
     int choice;
         cout<<"-----Simulation Dashboard-----"<<endl;
         cout<<"1. Display City Traffic Network"<<endl;
@@ -198,37 +351,35 @@ int main(){
                city.displayRoad(); 
                break;
             case 2: 
-               traffic.display();
+              // traffic.display();
                break;
             case 3:
                
                break;
             case 4:
-               closure.display();
+              // closure.display();
                break;
             case 5:
                break;
             case 6:
-               closure.blockRoad();
+              // closure.blockRoad();
                break;
             case 7:
                break;
             case 8:
-               exit= true;
+              // exit= true;
                break;
             default:
                cout<<"Invalid Choice!"<<endl;
                break;
         }
         }
-    char start;
-    char end;
-    cout<<"Enter points: "<<endl;
-    cout<<"Start: ";
-    cin>>start;
-    cout<<"  End: ";
-    cin>>end;
-    city.ShortestPath(start, end);
-
-  return 0;
+    cout<<"Assigning shortest path to vehicles:"<<endl;
+    city.read_vehicle();
+    city.changeCurrent(4);
+    CongestionMonitoring roads;
+    city.read_And_addVehicle(roads);
+    roads.display();
+    return 0;
+    
 }
